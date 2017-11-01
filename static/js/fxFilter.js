a5a35716527d63f5d495e42b8a59f56670cb47ed
DogeSWL <lusen.w1387@gmail.com>
2017-10-31 17:00:06 -0700

    got fxFilter working

--- static/js/fxFilter.js
+++ static/js/fxFilter.js
@@ -1,11 +1,16 @@
 $('#fxFilter').keyup(function(){
    var aValue = $(this).val().toUpperCase();
-    if(aValue == ""){
-        $('.fxSymbols > button').show();
-    } else {
-        $('.fxSymbols > button').each(function(){
-            var text = $(this).text().toUpperCase();
-            (text.indexOf(aValue) >= 0) ? $(this).show() : $(this).hide();
-        });
+
+   // show all if no input in filter
+   if(aValue == ""){
+      $('.fxSymbols > button').show();
+   } else {
+      $('.fxSymbols > button').each(function(){
+         var text = $(this).text().toUpperCase();
+
+         // indexOf returns -1 if aValue not in text
+         // if indexOf returns -1 hide text, else show text
+         (text.indexOf(aValue) >= 0) ? $(this).show() : $(this).hide();
+      });
    };
 });
