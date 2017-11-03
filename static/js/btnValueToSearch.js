965e7bd3fb1ba01089cc241ad534c3ca8cc3414a
DogeSWL <lusen.w1387@gmail.com>
2017-11-02 20:50:54 -0700

    minor changes to btnValueSearch

--- static/js/btnValueToSearch.js
+++ static/js/btnValueToSearch.js
@@ -4,7 +4,7 @@ $('button').click(function() {
    var btnValue = $(this).val() + ",";
 
    // .val( function ): Function( Integer index, String value ) => String
-   // a function returning the value to set. this is the current element 
+   // a function returning the value to set. [this] is the current element 
    // receives the index position of the element in the set and the old value as arguments.
    $('#searchText').val(function(_, searchVal){
       if (searchVal.match(btnValue)){
