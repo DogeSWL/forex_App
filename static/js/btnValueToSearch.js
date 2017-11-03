91ef5e3c29f3e2f8e48830bbb47dcb85f6e859e7
DogeSWL <lusen.w1387@gmail.com>
2017-11-02 20:12:04 -0700

    added button toggle tand button value inputs

---
+++ static/js/btnValueToSearch.js
@@ -0,0 +1,15 @@
+$('button').click(function() {
+
+   // get value of button clicked
+   var btnValue = $(this).val() + ",";
+
+   // .val( function ): Function( Integer index, String value ) => String
+   // a function returning the value to set. this is the current element 
+   // receives the index position of the element in the set and the old value as arguments.
+   $('#searchText').val(function(_, searchVal){
+      if (searchVal.match(btnValue)){
+         return searchVal.replace(btnValue, "");
+      }
+      return searchVal+ btnValue
+   });
+});
