f24b335d371ce60027c1903ef2df6b579755fa3d
Sen Wei Lu <sen@Sens-MacBook-Pro.local>
2018-01-10 17:17:03 -0800

    fix issues with db unable to import and symRate popping an error if forex symbols not loaded initially

--- views/symRate.py
+++ views/symRate.py
@@ -1,33 +1,18 @@
 from flask import render_template
-from app import app, db
+from app import app
 from models import FxSymbols
-from utilFuncs import randFuncs, forex_forge
+from utilFuncs import randFuncs
 
 import datetime
 
 @app.route('/symRate', methods=['GET'])
 def symbPage():
-    fxSymb = FxSymbols.query.all()
-    aMsg = ""
 
-    # check to see if table is empty (empty sequences are false)
-    # if table is empty, popluate table with current Forge FX list and UTC time
-    if not fxSymb:
-        aMsg = "list is empty"
+    dbFxList = FxSymbols.query.first()
+    rtnFxList = randFuncs.lngStringToList(dbFxList.symbFx)
 
-        # get symbols from Forge, sort it alphabetically and then turn list into a string
-        add_FxSymb = randFuncs.listToLongString(sorted(forex_forge.getSymbols()))
-        add_utcStamp = datetime.datetime.now(datetime.timezone.utc)
-
-        fs = FxSymbols(symbFx=add_FxSymb, lastUpdated=add_utcStamp)
-        db.session.add(fs)
-        db.session.commit()
-    else:
-        dbFxList = FxSymbols.query.first()
-        rtnFxList = randFuncs.lngStringToList(dbFxList.symbFx)
-
-        #removeing empty item in row 0 of list
-        rtnFxList.pop(0)
+    #removeing empty item in row 0 of list
+    rtnFxList.pop(0)
 
     # current coordinated universal time(UTC)
     utcTime = datetime.datetime.now(datetime.timezone.utc)
