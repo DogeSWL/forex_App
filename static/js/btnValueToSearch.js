1648689a16b203141b9c79b6c1305ae650a1d2cc
Sen Wei Lu <lusen.w1387@gmail.com>
2018-02-06 22:08:35 -0800

    fix issue with removing first fx symbol from search list

--- static/js/btnValueToSearch.js
+++ static/js/btnValueToSearch.js
@@ -1,15 +1,27 @@
 $('button').click(function() {
 
    // get value of button clicked
-   var btnValue = $(this).val() + ",";
+   var btnValue = $(this).val();
 
    // .val( function ): Function( Integer index, String value ) => String
-   // a function returning the value to set. [this] is the current element 
+   // a function returning the value to set. [this] is the current element
    // receives the index position of the element in the set and the old value as arguments.
    $('#searchText').val(function(_, searchVal){
-      if (searchVal.match(btnValue)){
-         return searchVal.replace(btnValue, "");
-      }
-      return searchVal+ btnValue
+
+     if (searchVal.length == 0){
+       return btnValue;
+     }else {
+       if(searchVal.match(btnValue)){
+         var fxArr = searchVal.split(",");
+         var btnIndex = fxArr.indexOf(btnValue);
+
+         fxArr = searchVal.split(",");
+         fxArr.splice(btnIndex,1);
+
+         return fxArr.toString();
+         }else {
+           return searchVal + "," + btnValue;
+       }
+     }
    });
 });
